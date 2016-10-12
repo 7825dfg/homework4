435d4eec803d2774ba8892658b54330c3b6325d7
201244076 LDH <7825dfg@naver.com>
2016-10-12 18:34:54 +0900

    5번 문제 완료

--- homework4/src/homework4/Gugudan2.java
+++ homework4/src/homework4/Gugudan2.java
@@ -1,5 +1,44 @@
 package homework4;
 
+import java.util.Scanner;
+
 public class Gugudan2 {
 
+	int num, ggd;
+	String oof, sel;
+
+	void input(){
+		Scanner A = new Scanner(System.in);
+		
+		System.out.println("E:Â¦¼ö´Ü, O:È¦¼ö´Ü(Q:Á¾·á)");
+		this.sel = A.next();
+		output();
+	}
+	
+	void output(){
+		
+		int array[] = new int[9];
+		
+		if(sel.equals("E")){
+			for(int i=1; i<5; i++){
+				for(int j=1; j<10; j++){
+					array[j-1] = j * (i*2);
+					System.out.println(2*i+"*"+j);
+				}
+			}
+		}
+		
+		else if(sel.equals("O")){
+			for(int i=1; i<5; i++){
+				for(int j=1; j<10; j++){
+					array[j-1] = j * ((2*i)+1);
+					System.out.println(((2*i)+1)+"*"+j);
+				}
+			}
+		}
+		
+			
+		
+	}
+	
 }
