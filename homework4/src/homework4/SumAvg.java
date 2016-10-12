f820b59f24783a6b84f27e88a2125a3069033ed4
201244076 LDH <7825dfg@naver.com>
2016-10-12 18:34:26 +0900

    3번 문제 완료

--- homework4/src/homework4/SumAvg.java
+++ homework4/src/homework4/SumAvg.java
@@ -1,5 +1,38 @@
 package homework4;
 
-public class SumAvg {
+import java.util.Scanner;
 
+public class SumAvg {
+	int num, sum, count;
+	double avg;
+	String oof;
+	
+	void input(){
+		
+		Scanner A = new Scanner(System.in);
+		
+		System.out.println("숫자를 입력(Q:종료)");
+	    
+	    while(A.hasNextInt()){
+	    	System.out.println("숫자를 입력(Q:종료)");
+	    	this.num = A.nextInt();
+	    	this.sum += this.num;
+	    	this.count++;
+	    	if(false) break;
+	    }
+	    
+	   
+	    this.oof = A.next();
+		if(oof.equals("Q"))
+		output();		
+		
+	}
+	
+	void output(){
+		this.avg = this.sum/this.count;
+		System.out.println("합계는 "+sum+"이고, 평균은 "+avg+"입니다.");
+		
+		
+	}
+	
 }
