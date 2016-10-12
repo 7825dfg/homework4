a2c90f363d3307882fe6d826efd971166e828f5f
201244076 LDH <7825dfg@naver.com>
2016-10-12 18:33:44 +0900

    1번 문제 완료

--- homework4/src/homework4/SumOfNumbers.java
+++ homework4/src/homework4/SumOfNumbers.java
@@ -1,5 +1,21 @@
 package homework4;
 
-public class SumOfNumbers {
+import java.util.Scanner;
 
+public class SumOfNumbers {
+    int num;
+	int count;
+	void input(){
+	   Scanner A = new Scanner(System.in);
+	   System.out.println("숫자를 입력하세요.");
+	   this.num = A.nextInt();
+	   output();
+   }
+	
+	void output(){
+		for(int i=0; i <= num; i++){
+			this.count += i;
+		}
+		System.out.println("총합은 "+count+" 입니다.");
+	}
 }
